3103ca47b801596e54166c16231ee04cdf01f7c1
Ruth <ruthzinabu89@gmail.com>
2024-08-19 19:17:58 +0300

    Responsive_Landing_Page

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "30%";
+}
+function closeNavbar() {
+    document.getElementById("sideNavigationBar")
+         
+        .style.opacity ="0";
+}
