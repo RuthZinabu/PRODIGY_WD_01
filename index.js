ca3972cca786d66dbbd017c7320cf4975f597bc0
Ruth <ruthzinabu89@gmail.com>
2024-08-19 19:57:49 +0300

    Responsive_Landing_Page

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function closeNavbar() {
          
         .style.opacity ="0";
 }
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
